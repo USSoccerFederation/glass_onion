991ba9e04d8a791e0a843792ff961824ff65cf78
aeaswaran-ussf <aeaswaran@ussoccer.org>
2025-12-21 13:07:37 -0500

    formatting

--- src/glass_onion/team.py
+++ src/glass_onion/team.py
@@ -124,7 +124,9 @@ class TeamSyncEngine(SyncEngine):
                         inplace=True,
                     )
                     # update remainders check
-                    synced = sync_result.dropna(subset=[input1.id_field, input2.id_field])
+                    synced = sync_result.dropna(
+                        subset=[input1.id_field, input2.id_field]
+                    )
 
         # third pass: cosine similarity but we don't care about the threshold
         remaining_1 = TeamSyncableContent(
